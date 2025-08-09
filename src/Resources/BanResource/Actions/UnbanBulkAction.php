039f0a49a76e7c7f596d6ad83a808ce96e013ef5
aspirantzhang <admin@aspirantzhang.com>
2025-08-09 14:52:26 +0800

    feat: update labels and notifications to use translation strings in BanResource actions and forms

--- src/Resources/BanResource/Actions/UnbanBulkAction.php
+++ src/Resources/BanResource/Actions/UnbanBulkAction.php
@@ -23,7 +23,7 @@ class UnbanBulkAction extends BulkAction
     {
         parent::setUp();
 
-        $this->label(__('Unban'));
+        $this->label(__('filament-ban-manager::filament-ban-manager.button.unban'));
 
         $this->icon('heroicon-o-x-circle');
 
@@ -31,7 +31,7 @@ class UnbanBulkAction extends BulkAction
 
         $this->requiresConfirmation();
 
-        $this->successNotificationTitle(__('Unbanned successfully'));
+        $this->successNotificationTitle(__('filament-ban-manager::filament-ban-manager.info.unbanned_successfully'));
 
         $this->action(function (): void {
             $this->process(static function (Collection $records) {
